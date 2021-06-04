3ac147cb49333e4baeb5d41b6698c30e7e2edeb5
ajinkyajagdale21 <68507834+ajinkyajagdale21@users.noreply.github.com>
2021-06-04 14:41:09 +0530

    Update datareducer.js

--- src/datareducer.js
+++ src/datareducer.js
@@ -2,7 +2,7 @@ export const DataReducer=(state,action)=>{
  
     switch(action.type){
         case "DATA": return {...state,data:action.payload}; 
-        case "ADD_TO_CART" :return{...state,cart:action.payload};
+        case "ADD_TO_CART" :return {...state,cart:[...state.cart,action.payload]};
         default:
              return {state}
     }
